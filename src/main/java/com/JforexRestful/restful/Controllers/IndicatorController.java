012483a1acdbb6c460ebe68a504c759bf6e722da
mx1322 <mx1322@gmail.com>
2023-06-20 12:45:58 +0200

    add macd and stoch indicator

--- src/main/java/com/JforexRestful/restful/Controllers/IndicatorController.java
+++ src/main/java/com/JforexRestful/restful/Controllers/IndicatorController.java
@@ -1,13 +1,9 @@
 package com.JforexRestful.restful.Controllers;
 
 import com.JforexRestful.restful.Services.IndicatorService;
-import com.dukascopy.api.IIndicators.MaType;
-import com.dukascopy.api.IIndicators.AppliedPrice;
-import com.dukascopy.api.Instrument;
+import com.dukascopy.api.IIndicators;
 import com.dukascopy.api.JFException;
 import com.dukascopy.api.OfferSide;
-import com.dukascopy.api.Period;
-import com.dukascopy.api.Filter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -18,8 +14,40 @@ public class IndicatorController {
     @Autowired
     private IndicatorService indicatorService;
 
+    public IndicatorController(IndicatorService indicatorService) {
+        this.indicatorService = indicatorService;
+    }
+
     @GetMapping("/bollinger")
-    public String calculateBollinger() throws JFException {
-        return indicatorService.calculateBollinger();
+    public String calculateBollinger(
+            @RequestParam(required = false) OfferSide offerSide,
+            @RequestParam(required = false) IIndicators.AppliedPrice appliedPrice,
+            @RequestParam(required = false) Integer timePeriod,
+            @RequestParam(required = false) Double nbDevUp,
+            @RequestParam(required = false) Double nbDevDn,
+            @RequestParam(required = false) IIndicators.MaType maType,
+            @RequestParam(required = false) Integer shift) throws JFException {
+        return indicatorService.calculateBollinger(offerSide, appliedPrice, timePeriod, nbDevUp, nbDevDn, maType, shift);
+    }
+
+    @GetMapping("/stochastic")
+    public String calculateStochastic(
+            @RequestParam(required = false) Integer fastKPeriod,
+            @RequestParam(required = false) Integer slowKPeriod,
+            @RequestParam(required = false) IIndicators.MaType KmaType,
+            @RequestParam(required = false) Integer slowDPeriod,
+            @RequestParam(required = false) IIndicators.MaType DmaType,
+            @RequestParam(required = false) Integer shift) throws JFException {
+        return indicatorService.calculateStochastic(fastKPeriod, slowKPeriod, KmaType, slowDPeriod, DmaType, shift);
+    }
+
+    @GetMapping("/macd")
+    public String calculateMACD(
+            @RequestParam(required = false) IIndicators.AppliedPrice appliedPrice,
+            @RequestParam(required = false) Integer fastPeriod,
+            @RequestParam(required = false) Integer slowPeriod,
+            @RequestParam(required = false) Integer signalPeriod,
+            @RequestParam(required = false) Integer shift) throws JFException {
+        return indicatorService.calculateMACD(appliedPrice,fastPeriod, slowPeriod, signalPeriod, shift);
     }
 }
--- src/main/java/com/JforexRestful/restful/services/IndicatorService.java
+++ src/main/java/com/JforexRestful/restful/services/IndicatorService.java
@@ -14,6 +14,7 @@ import org.slf4j.Logger;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import java.util.LinkedHashMap;
 import java.util.Map;
+import java.util.Optional;
 
 @Service
 public class IndicatorService {
@@ -31,69 +32,118 @@ public class IndicatorService {
         this.coreService = coreService;
         try {
             this.feedDescriptor = FeedDescriptorUtils.createDefaultFeedDescriptor();
-            this.appliedPrice = AppliedPrice.CLOSE;
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
 
-    private Object defaultIfNull(Object value, Object defaultValue) {
-        return value != null ? value : defaultValue;
+    private <T> T defaultIfNull(T value, T defaultValue) {
+        return Optional.ofNullable(value).orElse(defaultValue);
     }
 
-    private String createJsonResponse(double[] bbands) {
+    private String createJsonResponse(Map<String, double[]> data) {
         ObjectMapper mapper = new ObjectMapper();
         String jsonResponse = "";
         try {
-            Map<String, Object> map = new LinkedHashMap<>();
-            map.put("Bollinger Bands", bbands);
-            jsonResponse = mapper.writeValueAsString(map);
+            jsonResponse = mapper.writeValueAsString(data);
         } catch (Exception e) {
             e.printStackTrace();
         }
         return jsonResponse;
     }
 
+    public String calculateBollinger(
+            OfferSide offerSide,
+            AppliedPrice appliedPrice,
+            Integer timePeriod,
+            Double nbDevUp,
+            Double nbDevDn,
+            IIndicators.MaType maType,
+            Integer shift) throws JFException {
 
-    public String calculateBollinger() throws JFException {
-        // Initializing parameters
-        Instrument instrument = Instrument.EURUSD;
-        Period period = Period.FIFTEEN_MINS;
-        OfferSide offerSide = OfferSide.BID;
-        AppliedPrice appliedPrice = AppliedPrice.CLOSE;
-        int timePeriod = 5;
-        int BBtimePeriod = 20;
-        double nbDevUp = 2.0;
-        double nbDevDn = 2.0;
-        IIndicators.MaType maType = IIndicators.MaType.SMA;
-        int shift = 0;
+        // Initialize default values if parameters are null or not defined
+
+        offerSide = defaultIfNull(offerSide, feedDescriptor.getOfferSide());
+        appliedPrice = defaultIfNull(appliedPrice, AppliedPrice.CLOSE);
+        timePeriod = defaultIfNull(timePeriod, 20);
+        nbDevUp = defaultIfNull(nbDevUp, 2.0);
+        nbDevDn = defaultIfNull(nbDevDn, 2.0);
+        maType = defaultIfNull(maType, IIndicators.MaType.SMA);
+        shift = defaultIfNull(shift, 0);
 
         IIndicators indicators = coreService.getIndicators();
         if (indicators == null) {
             throw new JFException("Failed to get indicators from context");
         }
 
-        maType = (IIndicators.MaType) defaultIfNull(maType, IIndicators.MaType.SMA);
-
-        LOGGER.info("calculateBollinger is called with: appliedPrice={}, BBtimePeriod={}, nbDevUp={}, nbDevDn={}, maType={}, shift={}",
-                appliedPrice, BBtimePeriod, nbDevUp, nbDevDn, maType, shift);
-
-        LOGGER.info("FeedDescriptor details: {}", feedDescriptor.toString());
-
-        //double[] bollinger = indicators.bbands(feedDescriptor, appliedPrice, feedDescriptor.getOfferSide(), BBtimePeriod, nbDevUp, nbDevDn, maType).calculate(shift);
-        double[] bbands = indicators.bbands(
-                instrument, period, offerSide, appliedPrice,
-                timePeriod, nbDevUp, nbDevDn, maType,
-                shift);
+        double[] bbands = indicators.bbands(feedDescriptor, appliedPrice, offerSide, timePeriod, nbDevUp, nbDevDn, maType).calculate(shift);
 
         LOGGER.info("calculateBollinger results: {}", bbands);
 
-        double[] bbands1 = indicators.bbands(feedDescriptor, appliedPrice, offerSide, timePeriod, nbDevUp, nbDevDn, maType).calculate(shift);
+        Map<String, double[]> response = new LinkedHashMap<>();
+        response.put("Bollinger Bands", bbands);
 
-        LOGGER.info("calculateBollinger results1: {}", bbands1);
+        return createJsonResponse(response);
 
+    }
 
-        //return createJsonResponse(new Object[]{"Bollinger Bands", BBtimePeriod, maType.name(), shift, bollinger});
-        return (createJsonResponse(bbands));
+    public String calculateStochastic(Integer fastKPeriod,
+                                      Integer slowKPeriod,
+                                      IIndicators.MaType KmaType,
+                                      Integer slowDPeriod,
+                                      IIndicators.MaType DmaType,
+                                      Integer shift) throws JFException {
+
+        fastKPeriod = defaultIfNull(fastKPeriod, 14);
+        slowKPeriod = defaultIfNull(slowKPeriod, 3);
+        KmaType = defaultIfNull(KmaType, IIndicators.MaType.SMA);
+        slowDPeriod = defaultIfNull(slowDPeriod, 3);
+        DmaType = defaultIfNull(DmaType, IIndicators.MaType.SMA);
+        shift = defaultIfNull(shift, 0);
+
+        IIndicators indicators = coreService.getIndicators();
+        if (indicators == null) {
+            throw new JFException("Failed to get indicators from context");
+        }
+
+        if(feedDescriptor == null) {
+            throw new JFException("FeedDescriptor is null");
+        }
+
+        double[] sto = indicators.stoch(feedDescriptor, feedDescriptor.getOfferSide(), fastKPeriod, slowKPeriod, KmaType, slowDPeriod, DmaType).calculate(shift);
+        LOGGER.info("calculateSto results: {}", sto);
+
+        LOGGER.info("FeedDescriptor details: {}", feedDescriptor.toString());
+
+        Map<String, double[]> response = new LinkedHashMap<>();
+        response.put("Stochastic", sto);
+
+        return createJsonResponse(response);
+    }
+
+    public String calculateMACD(AppliedPrice appliedPrice,
+                                Integer fastPeriod,
+                                Integer slowPeriod,
+                                Integer signalPeriod,
+                                Integer shift) throws JFException {
+        appliedPrice = defaultIfNull(appliedPrice, AppliedPrice.CLOSE);
+        fastPeriod = defaultIfNull(fastPeriod, 12);
+        slowPeriod = defaultIfNull(slowPeriod, 26);
+        signalPeriod = defaultIfNull(signalPeriod, 9);
+        shift = defaultIfNull(shift, 0);
+
+        IIndicators indicators = coreService.getIndicators();
+        if (indicators == null) {
+            throw new JFException("Failed to get indicators from context");
+        }
+
+        double[] macd = indicators.macd(feedDescriptor, appliedPrice, feedDescriptor.getOfferSide(), fastPeriod, slowPeriod, signalPeriod).calculate(shift);
+
+        LOGGER.info("calculateMACD results: {}", macd);
+
+        Map<String, double[]> response = new LinkedHashMap<>();
+        response.put("MACD", macd);
+
+        return createJsonResponse(response);
     }
 }
